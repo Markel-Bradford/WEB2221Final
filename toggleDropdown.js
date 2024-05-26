aa44ffb475045e45c3501170fb43002770bba24b
Markel-Bradford <113131063+Markel-Bradford@users.noreply.github.com>
2024-05-26 14:28:08 -0500

    Add files via upload

---
+++ toggleDropdown.js
@@ -0,0 +1,8 @@
+function toggleDropdown() {
+    var navMenu = document.getElementById("nav-menu");
+    if (navMenu.style.display === "block") {
+        navMenu.style.display = "none";
+    } else {
+        navMenu.style.display = "block";
+    }
+}
